77dee4c3d13aa9eb106757ebf597767dcb0273f9
ngvuong <vminguyen@ucdavis.edu>
2022-05-05 21:30:56 -0700

    Update user model with joi validation

--- models/user.js
+++ models/user.js
@@ -1,21 +1,22 @@
 import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
+import Joi from 'joi';
 
 const userSchema = new Schema({
   username: {
     type: String,
     unique: true,
-    required: true,
+    required: [true, 'Username is required'],
   },
   email: {
     type: String,
     unique: true,
-    required: true,
+    required: [true, 'Email is required'],
   },
   password: {
     type: String,
-    minlength: 6,
-    required: true,
+    minLength: [6, 'Password must be at least 6 characters long'],
+    required: [true, 'Password is required'],
   },
   profile: {
     type: String,
@@ -23,5 +24,37 @@ const userSchema = new Schema({
   friends: [{ type: Schema.Types.ObjectId, ref: 'User' }],
   posts: [{ type: Schema.Types.ObjectId, ref: 'Post' }],
 });
+
+userSchema.statics.validateUser = function (user) {
+  const schema = Joi.object({
+    username: Joi.string().min(3).max(20).required().messages({
+      'string.empty': 'Username is required',
+      'string.min': 'Username must be at least 3 characters long',
+      'string.max': 'Username must be less than 20 characters long',
+      'any.required': 'Username is required',
+    }),
+    email: Joi.string().email().required().messages({
+      'string.empty': 'Email is required',
+      'string.email': 'Email is not valid',
+      'any.required': 'Email is required',
+    }),
+    password: Joi.string().min(6).required().messages({
+      'string.empty': 'Password is required',
+      'string.min': 'Password must be at least 6 characters long',
+      'any.required': 'Password is required',
+    }),
+    profile: Joi.string()
+      .optional()
+      .messages({ 'string.empty': 'Profile is required' }),
+    friends: Joi.array()
+      .optional()
+      .messages({ 'array.empty': 'Friends is required' }),
+    posts: Joi.array()
+      .optional()
+      .messages({ 'array.empty': 'Posts is required' }),
+  });
+
+  return schema.validate(user, { abortEarly: false });
+};
 
 export default mongoose.models.User || mongoose.model('User', userSchema);
