02ce816bff3481396343f114470b5e494a5d061d
ngvuong <vminguyen@ucdavis.edu>
2022-05-16 19:58:39 -0700

    Update avatar to use user context

--- components/content/Avatar.js
+++ components/content/Avatar.js
@@ -1,22 +1,38 @@
+import { useState, useEffect } from 'react';
+import { useSession } from 'next-auth/react';
 import Image from 'next/image';
+import { useUser } from '../../contexts/userContext';
 
 import styles from '../../styles/Avatar.module.scss';
 
-function Avatar({ user }) {
+function Avatar() {
+  const [{ user: currentUser }] = useUser();
+  const [user, setUser] = useState(currentUser);
+  const { data: session } = useSession();
+
+  useEffect(() => {
+    if (!currentUser) {
+      setUser(session?.user);
+    } else {
+      setUser(currentUser);
+    }
+  }, [currentUser, session]);
+
   return (
     <>
-      {user?.image ? (
-        <Image
-          src={user.image}
-          className={styles.image}
-          width={50}
-          height={50}
-          quality={80}
-          alt='Profile avatar'
-        />
-      ) : (
-        <span className={styles.avatar}>{user.name[0]}</span>
-      )}
+      {user &&
+        (user?.image ? (
+          <Image
+            src={user.image}
+            className={styles.image}
+            width={50}
+            height={50}
+            quality={80}
+            alt='Profile avatar'
+          />
+        ) : (
+          <span className={styles.avatar}>{user?.name[0]}</span>
+        ))}
     </>
   );
 }
