eed5b6d2c52a42d5d7ebbb0bc660ff6520d1af79
ngvuong <vminguyen@ucdavis.edu>
2022-05-21 04:04:59 -0700

    Add password change api handler

---
+++ pages/api/user/password.js
@@ -0,0 +1,58 @@
+import bcrypt from 'bcryptjs';
+import User from '../../../models/user';
+import dbConnect from '../../../lib/db';
+
+export default async function handler(req, res) {
+  if (req.method !== 'PUT') {
+    return res.status(405).json({ error: 'Method not allowed' });
+  }
+
+  const { currentPassword, newPassword, confirmPassword, user_id } = req.body;
+
+  if (!currentPassword || !newPassword || !confirmPassword) {
+    return res.status(400).json({ error: 'Please fill out all fields' });
+  }
+
+  if (newPassword !== confirmPassword) {
+    return res.status(400).json({
+      error: 'New passwords do not match',
+    });
+  }
+
+  if (newPassword.length < 6) {
+    return res.status(400).json({
+      error: 'Password must be at least 6 characters',
+    });
+  }
+
+  try {
+    await dbConnect();
+    const user = await User.findById(user_id);
+
+    const isValidPassword = await bcrypt.compare(
+      currentPassword,
+      user.password
+    );
+
+    if (!isValidPassword) {
+      return res.status(400).json({ error: 'Invalid current password' });
+    }
+
+    const isSamePassword = await bcrypt.compare(newPassword, user.password);
+
+    if (isSamePassword) {
+      return res.status(400).json({ error: 'Please enter a new password' });
+    }
+
+    const hashedPassword = await bcrypt.hash(newPassword, 12);
+
+    user.password = hashedPassword;
+    await user.save();
+
+    return res.status(200).json({ message: 'Password updated successfully' });
+  } catch (err) {
+    return res.status(400).json({
+      error: err.message,
+    });
+  }
+}
