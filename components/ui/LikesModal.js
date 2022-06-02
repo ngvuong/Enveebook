e7ece07a66122bc95462f07929ae2073dbc1e8fa
ngvuong <vminguyen@ucdavis.edu>
2022-06-01 21:41:23 -0700

    Add LikesModal displaying users who liked

---
+++ components/ui/LikesModal.js
@@ -0,0 +1,33 @@
+import { forwardRef } from 'react';
+import Link from 'next/link';
+import Avatar from './Avatar';
+import Overlay from '../layout/Overlay';
+
+import { FaThumbsUp } from 'react-icons/fa';
+import styles from '../../styles/LikesModal.module.scss';
+
+const LikesModal = forwardRef(({ users, onClose }, ref) => {
+  return (
+    <Overlay>
+      <div className={styles.container} ref={ref}>
+        <FaThumbsUp />
+        <hr />
+        {users.map((user) => (
+          <div key={user._id} className={styles.profile}>
+            <Link href={`/profile/${user._id}`}>
+              <a>
+                <Avatar key={user.id} width='40' height='40' user={user} />
+              </a>
+            </Link>
+            <Link href={`/profile/${user._id}`}>{user.name}</Link>
+          </div>
+        ))}
+        <button className={styles.btn_close} onClick={onClose}>
+          ✕
+        </button>
+      </div>
+    </Overlay>
+  );
+});
+
+export default LikesModal;
