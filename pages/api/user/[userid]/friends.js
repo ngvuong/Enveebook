d5b9d9921ef4bfd9f98e99496bf7bbe2b3193e31
ngvuong <vminguyen@ucdavis.edu>
2022-06-07 23:55:01 -0700

    Add friends api handler, requesting, accepting

---
+++ pages/api/user/[userid]/friends.js
@@ -0,0 +1,53 @@
+import User from '../../../../models/user';
+import dbConnect from '../../../../lib/db';
+
+export default async function handler(req, res) {
+  if (req.method !== 'PUT') {
+    return res.status(405).json({ error: 'Method not allowed' });
+  }
+
+  try {
+    const { userid } = req.query;
+    const { current_user_id, type } = req.body;
+
+    await dbConnect();
+
+    if (type === 'request') {
+      await User.findByIdAndUpdate(
+        userid,
+        {
+          $push: {
+            friendRequests: {
+              $each: [current_user_id],
+              $position: 0,
+            },
+          },
+        },
+        { runValidators: false }
+      );
+
+      return res.status(200).json({ message: 'Friend request sent' });
+    } else if (type === 'accept') {
+      await User.findByIdAndUpdate(
+        userid,
+        {
+          $push: { friends: current_user_id },
+        },
+        { runValidators: false }
+      );
+
+      await User.findByIdAndUpdate(
+        current_user_id,
+        {
+          $push: { friends: userid },
+          $pull: { friendRequests: userid },
+        },
+        { runValidators: false }
+      );
+
+      return res.status(200).json({ message: 'Friend request accepted' });
+    }
+  } catch (err) {
+    return res.status(500).json({ error: err.message });
+  }
+}
