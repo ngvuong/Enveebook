41d339313795d7744ea4205a01f18e49b10ebf5d
ngvuong <vminguyen@ucdavis.edu>
2022-06-11 04:03:52 -0700

    Update UserCard mutual friends,  mutualfriendslist

--- components/ui/UserCard.js
+++ components/ui/UserCard.js
@@ -1,6 +1,8 @@
 import Link from 'next/link';
 import { toast } from 'react-toastify';
 import Avatar from './Avatar';
+import UserListModal from './UserListModal';
+import useClickOutside from '../../hooks/useClickOutside';
 import { useUser } from '../../contexts/userContext';
 
 import styles from '../../styles/UserCard.module.scss';
@@ -13,12 +15,15 @@ function UserCard({
   type = 'request',
 }) {
   const [currentUser] = useUser();
+  const { triggerRef, nodeRef, show, setShow } = useClickOutside(false);
 
   const mutualFriends = currentUser
-    ? user.friends
-        .filter((friend) => currentUser.friends.includes(friend._id))
-        .map((friend) => <span key={friend._id}>{friend.name}</span>)
+    ? user.friends.filter((friend) => currentUser.friends.includes(friend._id))
     : [];
+
+  const mutualFriendsList = mutualFriends.map((friend) => (
+    <span key={friend._id}>{friend.name}</span>
+  ));
 
   const onAccept = async () => {
     const data = await fetch(`/api/user/${user._id}/friends`, {
@@ -72,19 +77,28 @@ function UserCard({
       <Avatar width='200' height='200' flexSize='100%' user={user} />
       <div className={styles.cardInfo}>
         <Link href={`/profile/${user._id}`}>{user.name}</Link>
-        {mutualFriends.length > 0 ? (
+        {mutualFriendsList.length > 0 ? (
           <>
-            <p>
-              {`${mutualFriends.length} mutual ${
-                mutualFriends.length === 1 ? 'friend' : 'friends'
+            <p ref={triggerRef}>
+              {`${mutualFriendsList.length} mutual ${
+                mutualFriendsList.length === 1 ? 'friend' : 'friends'
               }`}
             </p>
             <div className={styles.mutualFriends}>
-              {mutualFriends.slice(0, 10)}{' '}
-              {mutualFriends.length > 10 && (
-                <span>and {mutualFriends.slice(10).length} more...</span>
+              {mutualFriendsList.slice(0, 10)}{' '}
+              {mutualFriendsList.length > 10 && (
+                <span>and {mutualFriendsList.slice(10).length} more...</span>
               )}
             </div>
+            {show && (
+              <UserListModal
+                users={mutualFriends}
+                currentUser={currentUser}
+                onClose={() => setShow(false)}
+                type='mutualFriends'
+                ref={nodeRef}
+              />
+            )}
           </>
         ) : (
           <p></p>
