f584b7e978bbb2218d0cca72e7a7720b067bb18c
ngvuong <vminguyen@ucdavis.edu>
2022-06-12 03:59:50 -0700

    Update friends page, friends recommendation logic

--- pages/friends.js
+++ pages/friends.js
@@ -1,20 +1,50 @@
+import { useState } from 'react';
 import { getSession } from 'next-auth/react';
+import FriendList from '../components/ui/FriendList';
 import UserCard from '../components/ui/UserCard';
 import User from '../models/User';
 import dbConnect from '../lib/db';
 
 import styles from '../styles/Friends.module.scss';
 
-function Friends({ user }) {
-  console.log(user);
+function Friends({ user, friendRecommendations }) {
+  const [friends, setFriends] = useState(user.friends);
+  const [requests, setRequests] = useState(user.friendRequests);
+  const [recommendations, setRecommendations] = useState(friendRecommendations);
+
   return (
     <div className={styles.container}>
-      <h2>Friend Requests</h2>
-      <section className={styles.requests}>
-        {user.friendRequests.map((user) => (
-          <UserCard user={user} />
-        ))}
-      </section>
+      <FriendList friends={friends} />
+      <div className={styles.wrapper}>
+        {requests.length > 0 && (
+          <section className={styles.requestsSection}>
+            <p>Friend Requests</p>
+            <div className={styles.requests}>
+              {requests.map((user) => (
+                <UserCard
+                  key={user._id}
+                  user={user}
+                  setRequests={setRequests}
+                  setFriends={setFriends}
+                />
+              ))}
+            </div>
+          </section>
+        )}
+        <section className={styles.recommendationsSection}>
+          <p>Friend Recommendations</p>
+          <div className={styles.recommendations}>
+            {recommendations.map((user) => (
+              <UserCard
+                key={user._id}
+                user={user}
+                setRecommendations={setRecommendations}
+                type='recommendation'
+              />
+            ))}
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
@@ -22,15 +52,59 @@ function Friends({ user }) {
 export async function getServerSideProps(context) {
   const session = await getSession(context);
 
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      },
+    };
+  }
+
   await dbConnect();
 
-  const user = await User.findById(session.user.id, { password: 0 })
-    .populate('friends', 'name image')
-    .populate('friendRequests', 'name image');
+  const user = await User.findById(session.user._id, { password: 0 })
+    .populate('friends', 'name image friends')
+    .populate({
+      path: 'friendRequests',
+      select: 'name image',
+      populate: { path: 'friends', select: 'name image friends' },
+    });
+
+  const getMutualFriends = (target) => {
+    return user.friends.filter((uf) =>
+      target.friends.some((tf) => tf._id.toString() === uf._id.toString())
+    );
+  };
+  const sortByMutualFriends = (a, b) => {
+    const aLength = getMutualFriends(a).length;
+    const bLength = getMutualFriends(b).length;
+
+    return aLength > bLength ? -1 : aLength < bLength ? 1 : 0;
+  };
+
+  const nonFriends = [
+    ...(await User.find(
+      {
+        _id: { $ne: user._id },
+        friends: { $ne: user._id },
+        friendRequests: { $ne: user._id },
+      },
+      'friends name image'
+    ).populate('friends', 'name image friends')),
+  ]
+    .filter(
+      (u) =>
+        !user.friendRequests.some((r) => r._id.toString() === u._id.toString())
+    )
+    .sort(sortByMutualFriends);
 
   return {
     props: {
       user: JSON.parse(JSON.stringify(user)),
+      friendRecommendations: JSON.parse(
+        JSON.stringify(nonFriends.slice(0, 10))
+      ),
     },
   };
 }
