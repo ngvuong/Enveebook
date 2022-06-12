c68566b86d63d122fa37179eaf55e732725f1e0b
ngvuong <vminguyen@ucdavis.edu>
2022-06-12 04:01:00 -0700

    Update home page server side get initial feed

--- pages/home.js
+++ pages/home.js
@@ -1,14 +1,16 @@
 import { getSession } from 'next-auth/react';
 import Feed from '../components/content/Feed';
 import NewPostBox from '../components/ui/NewPostBox';
+import Post from '../models/post';
+import dbConnect from '../lib/db';
 
 import styles from '../styles/Home.module.scss';
 
-function Home({ user }) {
+function Home({ user, posts }) {
   return (
     <div className={styles.home}>
       <NewPostBox user={user} />
-      <Feed user={user} />
+      <Feed user={user} posts={posts} />
     </div>
   );
 }
@@ -25,9 +27,38 @@ export async function getServerSideProps(context) {
     };
   }
 
+  await dbConnect();
+
+  const authorList = [session.user._id, ...session.user.friends];
+
+  const posts = await Post.find({ author: { $in: authorList } })
+    .populate('author', 'name image')
+    .populate({
+      path: 'comments',
+      populate: [
+        { path: 'author', select: 'name image' },
+        {
+          path: 'likes',
+          select: 'name image friends',
+        },
+        {
+          path: 'replies',
+          populate: [
+            { path: 'author', select: 'name image' },
+            { path: 'likes', select: 'name image friends' },
+          ],
+        },
+      ],
+    })
+    .populate('likes', 'name image friends')
+    .sort({
+      createdAt: -1,
+    });
+
   return {
     props: {
       user: session.user,
+      posts: JSON.parse(JSON.stringify(posts)),
     },
   };
 }
