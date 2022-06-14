637f4ccaeeb59a09b372f2fbc03156ee5e73fb33
ngvuong <vminguyen@ucdavis.edu>
2022-06-13 20:28:45 -0700

    Update Feed post user prop, error styles

--- components/content/Feed.js
+++ components/content/Feed.js
@@ -18,7 +18,7 @@ function Feed({ user, posts }) {
   return (
     <section className={styles.feed}>
       {feedPosts.map((post) => (
-        <Post key={post._id} post={post} setFeed={setFeed} />
+        <Post key={post._id} post={post} user={user} setFeed={setFeed} />
       ))}
     </section>
   );
