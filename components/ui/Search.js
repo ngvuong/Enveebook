eaa1f00a770c994693ea785869356a0e50ad0608
ngvuong <vminguyen@ucdavis.edu>
2022-06-16 22:14:59 -0700

    Update Search support arrow up/down selection

--- components/ui/Search.js
+++ components/ui/Search.js
@@ -1,20 +1,71 @@
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
+import Link from 'next/link';
 import useClickOutside from '../../hooks/useClickOutside';
 
 import { FaSearch } from 'react-icons/fa';
 import styles from '../../styles/Search.module.scss';
-import Link from 'next/link';
 
 function Search({ users }) {
   const [results, setResults] = useState(users);
-  const { show, setShow, nodeRef, triggerRef } = useClickOutside(false);
+  const [active, setActive] = useState(-1);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [originalSearchTerm, setOriginalSearchTerm] = useState('');
+  const linkRef = useRef(null);
+  const { show, setShow, nodeRef } = useClickOutside(false);
+
+  useEffect(() => {
+    if (!show) {
+      setActive(-1);
+
+      if (searchTerm) {
+        setResults(
+          users.filter((user) =>
+            user.name.toLowerCase().includes(searchTerm.toLowerCase())
+          )
+        );
+      } else setResults(users);
+    }
+  }, [show, searchTerm, users]);
 
   const filterResults = (e) => {
+    const value = e.target.value;
+    setActive(-1);
+    setShow(true);
+    setSearchTerm(value);
+    setOriginalSearchTerm(value);
     setResults(
       users.filter((user) =>
-        user.name.toLowerCase().startsWith(e.target.value.toLowerCase())
+        user.name.toLowerCase().includes(value.toLowerCase())
       )
     );
+  };
+
+  const onKeyDown = (e) => {
+    if (e.keyCode === 13 && active !== -1) {
+      linkRef.current.click();
+    }
+
+    if (e.keyCode === 38) {
+      setActive((prev) => {
+        const newActive = prev === -1 ? results.length - 1 : prev - 1;
+        setSearchTerm(
+          newActive === -1 ? originalSearchTerm : results[newActive].name
+        );
+
+        return newActive;
+      });
+    }
+
+    if (e.keyCode === 40) {
+      setActive((prev) => {
+        const newActive = prev === results.length - 1 ? -1 : prev + 1;
+        setSearchTerm(
+          newActive === -1 ? originalSearchTerm : results[newActive].name
+        );
+
+        return newActive;
+      });
+    }
   };
 
   return (
@@ -27,17 +78,25 @@ function Search({ users }) {
           <input
             type='text'
             id='searchInput'
-            // value={searchTerm}
-            onChange={filterResults}
-            onFocus={() => setShow(true)}
-            placeholder='Search'
-            ref={triggerRef}
+            value={searchTerm}
+            onInput={filterResults}
+            onFocus={() => (show ? null : setShow(true))}
+            onClick={() => (show ? null : setShow(true))}
+            onKeyDown={onKeyDown}
+            placeholder='Search users'
+            autoComplete='off'
+            spellCheck='false'
           />
           {show && (
             <div className={styles.results} ref={nodeRef}>
-              {results.map((user) => (
+              {results.map((user, index) => (
                 <Link key={user._id} href={`/profile/${user._id}`}>
-                  {user.name}
+                  <a
+                    className={active === index ? styles.active : undefined}
+                    ref={active === index ? linkRef : undefined}
+                  >
+                    {user.name}
+                  </a>
                 </Link>
               ))}
             </div>
