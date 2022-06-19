fdaf494d4866a8abd0a2208f1c22b3fabcf68fde
ngvuong <vminguyen@ucdavis.edu>
2022-06-19 04:00:43 -0700

    Update home page useEffect dependency

--- pages/home.js
+++ pages/home.js
@@ -10,7 +10,7 @@ import styles from '../styles/Home.module.scss';
 function Home({ user, posts, setActivePage }) {
   useEffect(() => {
     setActivePage('home');
-  }, []);
+  }, [setActivePage]);
 
   return (
     <div className={styles.home}>
