110884fddeaf73f18af723f60952387bcef6bf76
ngvuong <vminguyen@ucdavis.edu>
2022-06-22 03:36:09 -0700

    Update Avatar conditional link option

--- components/ui/Avatar.js
+++ components/ui/Avatar.js
@@ -4,37 +4,47 @@ import Image from 'next/image';
 
 import styles from '../../styles/Avatar.module.scss';
 
-function Avatar({ height, width, flexSize, user }) {
+function Avatar({ height, width, flexSize, user, link = true }) {
   const [isError, setIsError] = useState(false);
 
+  const avatar = (
+    <>
+      {user.image.url && !isError ? (
+        <Image
+          src={user.image.url}
+          className={styles.image}
+          width={width}
+          height={height}
+          quality={100}
+          alt='Profile avatar'
+          draggable='false'
+          onError={() => setIsError(true)}
+        />
+      ) : (
+        <span
+          className={styles.avatar}
+          style={{
+            width: flexSize ? flexSize : `${width}px`,
+            height: flexSize ? flexSize : `${height}px`,
+            fontSize: `${width / 2}px`,
+          }}
+        >
+          {user.name[0]}
+        </span>
+      )}
+    </>
+  );
+
   return (
-    <Link href={`/profile/${user._id}`}>
-      <a className={styles.link}>
-        {user.image.url && !isError ? (
-          <Image
-            src={user.image.url}
-            className={styles.image}
-            width={width}
-            height={height}
-            quality={100}
-            alt='Profile avatar'
-            draggable='false'
-            onError={() => setIsError(true)}
-          />
-        ) : (
-          <span
-            className={styles.avatar}
-            style={{
-              width: flexSize ? flexSize : `${width}px`,
-              height: flexSize ? flexSize : `${height}px`,
-              fontSize: `${width / 2}px`,
-            }}
-          >
-            {user.name[0]}
-          </span>
-        )}
-      </a>
-    </Link>
+    <>
+      {link ? (
+        <Link href={`/profile/${user._id}`}>
+          <a className={styles.link}>{avatar}</a>
+        </Link>
+      ) : (
+        avatar
+      )}
+    </>
   );
 }
 
