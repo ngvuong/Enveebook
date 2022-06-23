5c38bf557286a7e704cc90efc9d034f978b8819f
ngvuong <vminguyen@ucdavis.edu>
2022-06-22 21:56:59 -0700

    Add chat page, messaging using firebase

---
+++ pages/chat.js
@@ -0,0 +1,136 @@
+import { useState, useEffect } from 'react';
+import { getSession } from 'next-auth/react';
+import Avatar from '../components/ui/Avatar';
+import Chatroom from '../components/ui/Chatroom';
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  query,
+  orderBy,
+  where,
+} from 'firebase/firestore';
+import '../lib/firebase';
+import User from '../models/user';
+import dbConnect from '../lib/db';
+
+import { FaPenAlt } from 'react-icons/fa';
+import styles from '../styles/Chat.module.scss';
+
+function Chat({ currentUser, users, chats }) {
+  const [activeChat, setActiveChat] = useState(chats[0]);
+
+  const chatPortals = chats.map((chat) => {
+    const recipient =
+      users[chat.members.find((member) => member !== currentUser._id)];
+    return (
+      <div
+        className={styles.portal}
+        key={chat.id}
+        onClick={() => console.log('hi')}
+      >
+        <Avatar height='50' width='50' user={recipient} link={false} />
+        <p>{recipient.name}</p>
+      </div>
+    );
+  });
+  // useEffect(() => {
+  //   const searchQuery = query(
+  //     collection(getFirestore(), 'chats'),
+  //     where('members', 'array-contains', currentUser._id),
+  //     orderBy('updatedAt', 'desc')
+  //   );
+
+  //   getDocs(searchQuery).then((results) => {
+  //     const chatDocs = [];
+  //     results.forEach((doc) => {
+  //       chatDocs.push(doc.data());
+  //     });
+
+  //     setChats(chatDocs);
+  //   });
+  // }, [currentUser]);
+  // console.log(chats);
+  return (
+    <div className={styles.container}>
+      <section className={styles.sideBar}>
+        <div className={styles.sideBarHead}>
+          <h1>Chats</h1>
+          <FaPenAlt />
+        </div>
+        <div className={styles.portals}>{chatPortals}</div>
+      </section>
+      <section className={styles.chatroom}>
+        {activeChat ? (
+          <Chatroom
+            chat={activeChat}
+            recipient={
+              users[
+                activeChat.members.find((member) => member !== currentUser._id)
+              ]
+            }
+            currentUser={currentUser}
+          />
+        ) : null}
+      </section>
+    </div>
+  );
+}
+
+export async function getServerSideProps(context) {
+  context.res.setHeader(
+    'Cache-Control',
+    'public, s-maxage=10, stale-while-revalidate=600'
+  );
+
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      },
+    };
+  }
+
+  const searchQuery = query(
+    collection(getFirestore(), 'chats'),
+    where('members', 'array-contains', session.user._id),
+    orderBy('updatedAt', 'desc')
+  );
+
+  const chats = [];
+  const userIds = [];
+
+  const querySnapshot = await getDocs(searchQuery);
+  querySnapshot.forEach((doc) => {
+    const docData = doc.data();
+    docData.id = doc.id;
+    docData.members.forEach((id) => {
+      if (!userIds.includes(id)) {
+        userIds.push(id);
+      }
+    });
+    chats.push(docData);
+  });
+
+  await dbConnect();
+
+  const users = [
+    ...(await User.find({ _id: { $in: userIds } }, 'name image')),
+  ].reduce((acc, curr) => {
+    acc[curr._id] = curr;
+    return acc;
+  }, {});
+
+  return {
+    props: {
+      currentUser: session.user,
+      users: JSON.parse(JSON.stringify(users)),
+      chats: JSON.parse(JSON.stringify(chats)),
+    },
+  };
+}
+
+export default Chat;
