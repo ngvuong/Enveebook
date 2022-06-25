f7efc92e27e710362f2c8592b0ff7cfef647859f
ngvuong <vminguyen@ucdavis.edu>
2022-06-25 03:59:47 -0700

    Update usePosts hook with options param

--- hooks/usePosts.js
+++ hooks/usePosts.js
@@ -1,9 +1,7 @@
 import useSWR from 'swr';
 
-function usePosts(userId, fallbackData) {
-  const { data, error, mutate } = useSWR(`/api/user/${userId}/posts`, {
-    fallbackData,
-  });
+function usePosts(userId, options) {
+  const { data, error, mutate } = useSWR(`/api/user/${userId}/posts`, options);
 
   return {
     posts: data,
